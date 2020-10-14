c15537ef525f5ae1e338c76bcd2b7fbbccf3fe16
Dems972 <nicolas.m972@gmail.com>
2020-10-14 10:12:55 +0200

    Feature Portal ok

---
+++ Assets/Dems/ScriptsDems/PortalScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalScript : MonoBehaviour
+{
+    [Header("Parameters")]
+    public int m_deplacement = 3;
+
+    
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(this.name== "Quad_Portal_A")
+        {
+            _destination = GameObject.Find("Quad_Portal_B").transform.position;
+        }
+        else
+        {
+            _destination = GameObject.Find("Quad_Portal_A").transform.position;
+        }
+
+        other.transform.position = _destination - Vector3.forward * m_deplacement;
+        other.transform.Rotate(Vector3.up * 180);
+    }
+
+    private Vector3 _destination;
+}
