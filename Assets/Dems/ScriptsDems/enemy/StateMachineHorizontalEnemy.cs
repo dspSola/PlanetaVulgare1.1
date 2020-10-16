2aea0ef3e57b29e4e905d5f344782b94c0349e71
Dems972 <nicolas.m972@gmail.com>
2020-10-16 20:11:38 +0200

    Enemy first version

---
+++ Assets/Dems/ScriptsDems/enemy/StateMachineHorizontalEnemy.cs
@@ -0,0 +1,300 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnemyHorizontalState
+{
+    IDLE,
+    SNEAKING,
+    JOGGING,
+    RUNNING
+}
+
+
+public class StateMachineHorizontalEnemy : MonoBehaviour
+{
+
+    [SerializeField] private EnemyHorizontalState _currentState;
+
+    public EnemyHorizontalState CurrentState
+    {
+        get
+        {
+            return _currentState;
+        }
+    }
+
+    private void Start()
+    {
+        TransitionToState(_currentState, EnemyHorizontalState.IDLE);
+    }
+
+    private void Update()
+    {
+        DoUpdate();
+    }
+
+    public void DoUpdate()
+    {
+        OnStateUpdate(_currentState);
+    }
+
+    #region State Machine
+
+    private void OnStateEnter(EnemyHorizontalState state)
+    {
+        switch (state)
+        {
+            case EnemyHorizontalState.IDLE:
+                DoIdleEnter();
+                break;
+
+            case EnemyHorizontalState.SNEAKING:
+                DoSneakingEnter();
+                break;
+
+            case EnemyHorizontalState.JOGGING:
+                DoJoggingEnter();
+                break;
+
+            case EnemyHorizontalState.RUNNING:
+                DoRunningEnter();
+                break;
+
+            default:
+                Debug.LogError("OnStateEnter: Invalid state " + state.ToString());
+                break;
+        }
+    }
+
+    private void OnStateExit(EnemyHorizontalState state)
+    {
+        switch (state)
+        {
+            case EnemyHorizontalState.IDLE:
+                DoIdleExit();
+                break;
+
+            case EnemyHorizontalState.SNEAKING:
+                DoSneakingExit();
+                break;
+
+            case EnemyHorizontalState.JOGGING:
+                DoJoggingExit();
+                break;
+
+            case EnemyHorizontalState.RUNNING:
+                DoRunningExit();
+                break;
+
+            default:
+                Debug.LogError("OnStateExit: Invalid state " + state.ToString());
+                break;
+        }
+    }
+
+    private void OnStateUpdate(EnemyHorizontalState state)
+    {
+        switch (state)
+        {
+            case EnemyHorizontalState.IDLE:
+                DoIdleUpdate();
+                break;
+
+            case EnemyHorizontalState.SNEAKING:
+                DoSneakingUpdate();
+                break;
+
+            case EnemyHorizontalState.JOGGING:
+                DoJoggingUpdate();
+                break;
+
+            case EnemyHorizontalState.RUNNING:
+                DoRunningUpdate();
+                break;
+
+            default:
+                Debug.LogError("OnStateUpdate: Invalid state " + state.ToString());
+                break;
+        }
+    }
+
+    private void TransitionToState(EnemyHorizontalState fromState, EnemyHorizontalState toState)
+    {
+        OnStateExit(fromState);
+        _currentState = toState;
+        OnStateEnter(toState);
+    }
+
+    private void TransitionToState(EnemyHorizontalState toState)
+    {
+        TransitionToState(_currentState, toState);
+    }
+    #endregion
+
+    #region State Idle
+
+    private void DoIdleEnter()
+    {
+        //_playerMove.DoIdle();
+        //_bruteAnimatorController.SetIdle(true);
+    }
+
+    private void DoIdleExit()
+    {
+        //_bruteAnimatorController.SetIdle(false);
+    }
+
+    private void DoIdleUpdate()
+    {
+        //if (_getBruteInput.SneakInput.IsActive)
+        //{
+        //    TransitionToState(EnemyHorizontalState.SNEAKING);
+        //    return;
+        //}
+
+        //if (_getBruteInput.Movement.sqrMagnitude > 0.01f)
+        //{
+        //    if (_getBruteInput.RunInput.IsActive && _getBruteInput.Movement.z > 0.01f)
+        //    {
+        //        TransitionToState(EnemyHorizontalState.RUNNING);
+        //        return;
+        //    }
+
+        //    TransitionToState(EnemyHorizontalState.JOGGING);
+        //    return;
+        //}
+    }
+
+    #endregion
+
+
+    #region State Sneaking
+
+    private void DoSneakingEnter()
+    {
+        //_playerMove.DoSneak();
+        //_bruteAnimatorController.SetSneacking(true);
+    }
+
+    private void DoSneakingExit()
+    {
+        //_bruteAnimatorController.SetSneacking(false);
+    }
+
+    private void DoSneakingUpdate()
+    {
+        //if (_getBruteInput.SneakInput.IsUp)
+        //{
+        //    if (_getBruteInput.Movement.sqrMagnitude > 0.01f)
+        //    {
+        //        if (_getBruteInput.RunInput.IsActive)
+        //        {
+        //            TransitionToState(EnemyHorizontalState.RUNNING);
+        //            return;
+        //        }
+
+        //        TransitionToState(EnemyHorizontalState.JOGGING);
+        //        return;
+        //    }
+        //    else
+        //    {
+        //        TransitionToState(EnemyHorizontalState.IDLE);
+        //        return;
+        //    }
+        //}
+    }
+
+    #endregion
+
+
+    #region State Jogging
+
+    private void DoJoggingEnter()
+    {
+        //_playerMove.DoJog();
+        //_bruteAnimatorController.SetWalking(true);
+    }
+
+    private void DoJoggingExit()
+    {
+        //_bruteAnimatorController.SetWalking(false);
+    }
+
+    private void DoJoggingUpdate()
+    {
+        //if (_getBruteInput.Movement.sqrMagnitude > 0.01f)
+        //{
+        //    if (_getBruteInput.SneakInput.IsActive)
+        //    {
+        //        TransitionToState(EnemyHorizontalState.SNEAKING);
+        //        return;
+        //    }
+
+        //    if (_getBruteInput.RunInput.IsActive && _getBruteInput.Movement.z > 0.01f)
+        //    {
+        //        TransitionToState(EnemyHorizontalState.RUNNING);
+        //        return;
+        //    }
+        //}
+        //else
+        //{
+        //    TransitionToState(EnemyHorizontalState.IDLE);
+        //    return;
+        //}
+    }
+
+    #endregion
+
+
+    #region State Running
+
+    private void DoRunningEnter()
+    {
+        //_playerMove.DoRun();
+        //_bruteAnimatorController.SetRunning(true);
+    }
+
+    private void DoRunningExit()
+    {
+        //_bruteAnimatorController.SetRunning(false);
+    }
+
+    private void DoRunningUpdate()
+    {
+        //if (_getBruteInput.Movement.sqrMagnitude > 0.01f)
+        //{
+        //    if (_getBruteInput.SneakInput.IsActive)
+        //    {
+        //        TransitionToState(EnemyHorizontalState.SNEAKING);
+        //        return;
+        //    }
+
+        //    if (_getBruteInput.RunInput.IsUp)
+        //    {
+        //        TransitionToState(EnemyHorizontalState.JOGGING);
+        //        return;
+        //    }
+
+        //    if (_getBruteInput.Movement.z < 0.01f)
+        //    {
+        //        TransitionToState(EnemyHorizontalState.JOGGING);
+        //        return;
+        //    }
+        //}
+        //else
+        //{
+        //    TransitionToState(EnemyHorizontalState.IDLE);
+        //    return;
+        //}
+    }
+
+    #endregion
+
+
+    #region Private
+
+    
+
+    #endregion
+}
---
+++ Assets/Dems/ScriptsDems/enemy/StateMachineVerticalEnemy.cs
@@ -0,0 +1,216 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnemyVerticalState
+{
+    GROUNDED,
+    JUMPING,
+    FALLING,
+}
+
+
+public class StateMachineVerticalEnemy : MonoBehaviour
+{
+    [SerializeField] private EnemyVerticalState _currentState;
+    [SerializeField] private CollisionOverlapBoxTester _groundCheck;
+
+    #region Public properties
+
+    public EnemyVerticalState CurrentState
+    {
+        get
+        {
+            return _currentState;
+        }
+    }
+
+    #endregion
+
+    #region Unity Lifecycle
+
+    private void Start()
+    {
+        TransitionToState(_currentState, EnemyVerticalState.FALLING);
+    }
+
+    private void Update()
+    {
+        DoUpdate();
+    }
+
+    public void DoUpdate()
+    {
+        OnStateUpdate(_currentState);
+    }
+    #endregion
+
+    #region State Machine
+
+    private void OnStateEnter(EnemyVerticalState state)
+    {
+        switch (state)
+        {
+            case EnemyVerticalState.GROUNDED:
+                DoGroundedEnter();
+                break;
+
+            case EnemyVerticalState.JUMPING:
+                DoJumpingEnter();
+                break;
+
+            case EnemyVerticalState.FALLING:
+                DoFallingEnter();
+                break;
+
+            default:
+                Debug.LogError("OnStateEnter: Invalid state " + state.ToString());
+                break;
+        }
+    }
+
+    private void OnStateExit(EnemyVerticalState state)
+    {
+        switch (state)
+        {
+            case EnemyVerticalState.GROUNDED:
+                DoGroundedExit();
+                break;
+
+            case EnemyVerticalState.JUMPING:
+                DoJumpingExit();
+                break;
+
+            case EnemyVerticalState.FALLING:
+                DoFallingExit();
+                break;
+
+            default:
+                Debug.LogError("OnStateExit: Invalid state " + state.ToString());
+                break;
+        }
+    }
+
+    private void OnStateUpdate(EnemyVerticalState state)
+    {
+        switch (state)
+        {
+            case EnemyVerticalState.GROUNDED:
+                DoGroundedUpdate();
+                break;
+
+            case EnemyVerticalState.JUMPING:
+                DoJumpingUpdate();
+                break;
+
+            case EnemyVerticalState.FALLING:
+                DoFallingUpdate();
+                break;
+
+            default:
+                Debug.LogError("OnStateUpdate: Invalid state " + state.ToString());
+                break;
+        }
+    }
+
+    private void TransitionToState(EnemyVerticalState fromState, EnemyVerticalState toState)
+    {
+        OnStateExit(fromState);
+        _currentState = toState;
+        OnStateEnter(toState);
+    }
+
+    private void TransitionToState(EnemyVerticalState toState)
+    {
+        TransitionToState(_currentState, toState);
+    }
+
+    #endregion
+
+    #region State Grounded
+
+    private void DoGroundedEnter()
+    {
+        //_bruteAnimatorController.SetGrounded(true);
+    }
+
+    private void DoGroundedExit()
+    {
+        //_bruteAnimatorController.SetGrounded(false);
+    }
+
+    private void DoGroundedUpdate()
+    {
+        if (!_groundCheck.TestCollision())
+        {
+            TransitionToState(EnemyVerticalState.FALLING);
+            return;
+        }
+
+        //if (_getBruteInput.JumpInput.IsActive)
+        //{
+        //    TransitionToState(EnemyVerticalState.JUMPING);
+        //    return;
+        //}
+    }
+
+    #endregion
+
+    #region State Jumping
+
+    private void DoJumpingEnter()
+    {
+        //_playerMove.DoJump();
+        //_bruteAnimatorController.SetJumping(true);
+    }
+
+    private void DoJumpingExit()
+    {
+        //_bruteAnimatorController.SetJumping(false);
+    }
+
+    private void DoJumpingUpdate()
+    {
+        //if (_playerMove.VelocityRb.y < -0.01f)
+        //{
+        //    TransitionToState(EnemyVerticalState.FALLING);
+        //    return;
+        //}
+    }
+
+    #endregion
+
+    #region State Falling
+
+    private void DoFallingEnter()
+    {
+        //_bruteAnimatorController.SetFalling(true);
+        Debug.Log(" DoFallingEnter ");
+    }
+
+    private void DoFallingExit()
+    {
+        //_bruteAnimatorController.SetFalling(false);
+        Debug.Log(" DoFallingEXIT ");
+
+    }
+
+    private void DoFallingUpdate()
+    {
+        if (_groundCheck.TestCollision())
+        {
+            TransitionToState(EnemyVerticalState.GROUNDED);
+            return;
+        }
+    }
+
+    #endregion
+
+
+
+    #region Private
+
+   
+
+    #endregion
+}
