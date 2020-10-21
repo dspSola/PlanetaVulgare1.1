14858dc4bceec5f5ac5a86512073a625029f99ad
DspSola <robin.sola@gmail.com>
2020-10-21 15:21:59 +0200

    Scene main (en cour)

---
+++ Assets/01_Scripts/Other/LoadScenes.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScenes : MonoBehaviour
+{
+    private void Awake()
+    {
+        private void Awake()
+        {
+            SceneManager.LoadScene("Camera", LoadSceneMode.Additive);
+            SceneManager.LoadScene("Audio", LoadSceneMode.Additive);
+            SceneManager.LoadScene("UI", LoadSceneMode.Additive);
+            SceneManager.LoadScene("Level", LoadSceneMode.Additive);
+            SceneManager.LoadScene("Enemys", LoadSceneMode.Additive);
+            SceneManager.LoadScene("Player", LoadSceneMode.Additive);
+        }
+    }
+}
