3039e83711f8eb343db5db1dd673daa14c31308b
Victor DS <vsenneville@yahoo.fr>
2020-10-23 03:27:24 +0200

    Implémentation portail

--- Assets/01_Scripts/PortalScrip/PortalScript.cs
+++ Assets/01_Scripts/PortalScrip/PortalScript.cs
@@ -8,12 +8,16 @@ public class PortalScript : MonoBehaviour
     public int m_deplacement = 3;
 
 
-    [SerializeField] Material _bloodSun;
-    [SerializeField] Material _sunnySun;
-
+    [SerializeField] Material _bloodSunSkybox;
+    [SerializeField] Material _sunnySunSkybox;
+    [SerializeField] GameObject _bloodSun;
+    [SerializeField] GameObject _sunnySun;
+ 
     private void Start()
     {
-        RenderSettings.skybox = _bloodSun;
+        RenderSettings.skybox = _bloodSunSkybox;
+        _bloodSun.SetActive(true);
+        _sunnySun.SetActive(false);
     }
 
 
@@ -22,19 +26,26 @@ public class PortalScript : MonoBehaviour
     {   
         //if (other.gameObject.CompareTag("PlayerColl"))
         //{
-            other.transform.position = _destination - Vector3.forward * m_deplacement;
-            other.transform.Rotate(Vector3.up * 180);
+            
 
-            if(this.name== "Quad_Portal_A")
-            {
-                _destination = GameObject.Find("Quad_Portal_B").transform.position;
-                RenderSettings.skybox = _sunnySun;
-            }
-            else
-            {
-                _destination = GameObject.Find("Quad_Portal_A").transform.position;
-                RenderSettings.skybox = _bloodSun;
-            }
+        if(this.name== "Quad_Portal_A")
+        {
+            _destination = GameObject.Find("Quad_Portal_B").transform.position;
+            RenderSettings.skybox = _sunnySunSkybox;
+            _sunnySun.SetActive(true);
+            _bloodSun.SetActive(false);
+
+        }
+        else
+        {
+            _destination = GameObject.Find("Quad_Portal_A").transform.position;
+            RenderSettings.skybox = _bloodSunSkybox;
+            _sunnySun.SetActive(false);
+            _bloodSun.SetActive(true);
+        }
+
+        other.transform.position = _destination + Vector3.forward * m_deplacement;
+        other.transform.Rotate(Vector3.up*90);
 
         //}
     }
