0738621c3d795f84d3d433e661d4dc30eb5983c2
Nicolas <nicolas.m972@gmail.com>
2020-10-23 05:41:01 +0200

    Suite de ui
    ui cntinué

--- Assets/01_Scripts/Menu/MenuPause.cs
+++ Assets/01_Scripts/Menu/MenuPause.cs
@@ -12,7 +12,7 @@ public class MenuPause : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape ))
+        if(Input.GetKeyDown(KeyCode.Escape) || Input.GetButtonDown("Pause"))
         {
             if(m_gameIsPaused)
             {
