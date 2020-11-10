e35659be7d9b6800313e741993a152dbb41d1b81
DspSola <robin.sola@gmail.com>
2020-11-10 18:26:16 +0100

    tree debut suite

---
+++ Assets/01_Scripts/Enemy/TreeMutant/TreeMutantAnimationControler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class TreeMutantAnimationControler : MonoBehaviour
+{
+    [SerializeField] Animator _animator;
+
+    private void Awake()
+    {
+        _transform = transform;
+    }
+
+    private void Update()
+    {
+        _animator.SetFloat(_moveZId, _transform.position.z);
+    }
+
+    int _moveZId = Animator.StringToHash("MoveZ");
+
+    Transform _transform;
+}
--- Assets/01_Scripts/Enemy/TreeMutant/TreeMutantMovement.cs
+++ Assets/01_Scripts/Enemy/TreeMutant/TreeMutantMovement.cs
@@ -3,17 +3,37 @@ using UnityEngine.AI;
 
 public class TreeMutantMovement : MonoBehaviour
 {
-    [SerializeField] NavMeshSurface _navMeshSurface;
+    [SerializeField] float _radius;
 
     private void Awake()
     {
         _transform = transform;
+        _navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     private void Update()
     {
-        
+        if(_navMeshAgent.destination != null)
+        {
+            RandomNavmeshLocation(_radius);
+        }
+    }
+
+    public Vector3 RandomNavmeshLocation(float radius)
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * radius;
+        randomDirection += _transform.position;
+        NavMeshHit hit;
+        Vector3 finalPosition = Vector3.forward/*= Vector3.zero*/;
+
+        if (NavMesh.SamplePosition(randomDirection, out hit, radius, 1))
+        {
+            finalPosition = hit.position;
+        }
+
+        return finalPosition;
     }
 
     Transform _transform;
+    NavMeshAgent _navMeshAgent;
 }
